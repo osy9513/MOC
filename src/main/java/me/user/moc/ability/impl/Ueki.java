bb80b1d3f0c349bd08eb4fb7d6bf5b3279f3d463
one_c <sy.oh@impix.com>
2026-01-25 14:37:29 +0900

    2026.01.25 sy.oh (2) 능력 우에키 - 쿨타임 및 능력 구현 완료.

--- src/main/java/me/user/moc/ability/impl/Ueki.java
+++ src/main/java/me/user/moc/ability/impl/Ueki.java
@@ -15,6 +15,8 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Location;
+import org.bukkit.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -24,7 +26,7 @@ public class Ueki extends Ability {
 
     // 플레이어별 쿨타임을 저장하는 장부입니다. (단위: 밀리초)
     private final HashMap<UUID, Long> cooldowns = new HashMap<>();
-    private final int COOLDOWN_TIME = 7; // 쿨타임 15초
+    private final int COOLDOWN_TIME = 12; // 쿨타임 12초
 
     public Ueki(JavaPlugin plugin) {
         super(plugin);
@@ -57,7 +59,7 @@ public class Ueki extends Ability {
                 "\n" +
                 "나무로 변합니다.\n" +
                 "\n" +
-                "쿨타임 : 7초.\n" +
+                "쿨타임 : 12초.\n" +
                 "\n" +
                 "---\n" +
                 "\n" +
@@ -91,15 +93,46 @@ public class Ueki extends Ability {
     }
 
     private void useAbility(Player p) {
-        p.sendMessage("§a[!] 쓰레기를 나무로 바꾸는 힘!");
+        Bukkit.broadcastMessage("§a 우에키 : 쓰레기를 나무로 바꾸는 힘!");
+
         p.playSound(p.getLocation(), Sound.BLOCK_CHERRY_SAPLING_PLACE, 1f, 1f);
 
         // 주변 20블럭 이내의 아이템과 생명체 탐색
         for (Entity entity : p.getNearbyEntities(20, 20, 20)) {
+            // 아이템이거나, 시전자가 아닌 생명체인 경우
             if (entity instanceof Item || (entity instanceof LivingEntity && !entity.equals(p))) {
-                // 해당 위치에 나무(참나무 로그) 설치
-                entity.getLocation().getBlock().setType(Material.OAK_LOG);
-                if (!(entity instanceof Player)) entity.remove(); // 플레이어가 아니면 제거
+
+                // [▼▼▼ 여기서부터 변경됨: 블록 설치 대신 나무 생성 로직으로 변경 ▼▼▼]
+                Location loc = entity.getLocation();
+
+                // 1. 나무가 어떤 블럭 위에서도 잘 자라도록 발바닥 블럭을 흙으로 바꿉니다.
+                loc.getBlock().getRelative(0, -1, 0).setType(Material.GRASS_BLOCK);
+
+                // 2. 해당 위치에 진짜 나무(참나무) 한 그루를 생성합니다.
+                // generateTree는 공간이 부족하면 생성되지 않을 수 있으므로,
+                // 더 확실하게 생성되도록 일반 TREE 타입을 사용합니다.
+                boolean grown = loc.getWorld().generateTree(loc, org.bukkit.TreeType.TREE);
+
+                // 만약 공간 문제로 나무가 생성되지 않았다면, 최소한 로그 블록이라도 설치해서 가둡니다.
+                // 3. 만약 공간이 좁아 나무가 안 자랐다면? 우리가 직접 "강제로" 만듭니다!
+                if (!grown) {
+                    // 기둥 세우기 (3칸 높이)
+                    for (int i = 0; i < 3; i++) {
+                        loc.clone().add(0, i, 0).getBlock().setType(Material.OAK_LOG);
+                    }
+                    // 잎사귀 씌우기 (기둥 꼭대기와 옆면에 십자 모양으로 설치)
+                    loc.clone().add(0, 3, 0).getBlock().setType(Material.OAK_LEAVES); // 맨 위
+                    loc.clone().add(1, 2, 0).getBlock().setType(Material.OAK_LEAVES); // 동
+                    loc.clone().add(-1, 2, 0).getBlock().setType(Material.OAK_LEAVES); // 서
+                    loc.clone().add(0, 2, 1).getBlock().setType(Material.OAK_LEAVES); // 남
+                    loc.clone().add(0, 2, -1).getBlock().setType(Material.OAK_LEAVES); // 북
+                }
+
+                // 3. 대상이 플레이어가 아니면(아이템이나 몹) 제거합니다.
+                if (!(entity instanceof Player)) {
+                    entity.remove();
+                }
+                // [▲▲▲ 여기까지 변경됨 ▲▲▲]
             }
         }
     }
